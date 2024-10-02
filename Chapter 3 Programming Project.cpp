7651694a972bdfdc9d74d20dd94848c0e63c4b80
jjw86297 <jjw86297@email.vccs.edu>
2024-10-02 17:18:01 -0400

    First and Final Commit

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -1,20 +1,24 @@
-// Chapter 3 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Jacob Walrath
+//10/2/2024
+//Chapter 3 Programming Project
+//Math_Tutor
 
 #include <iostream>
+#include <random>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    random_device myEngine;
+    uniform_int_distribution<int> randomInt(0, 1000);
+
+    int number1 = randomInt(myEngine);
+    int number2 = randomInt(myEngine);
+
+    cout << "What is " << number1 << " + " << number2 << "?" << endl << "Press Enter for the answer.";
+    cin.get();
+
+    cout << number1 + number2;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
